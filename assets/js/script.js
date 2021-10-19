82fcb944938a581f350561515f7afbd0244ba855
scorpion77777 <vvmartirosyan@gmail.com>
2021-10-19 13:43:22 -0700

    innitial commit

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+var timeDisplayEl = $("#time-display");
+
+var todoInput = document.querySelector("#todo-text");
+var todoForm = document.querySelector("#todo-form");
+var saveButton = document.querySelector("#submit");
+var todoCountSpan = document.querySelector("#todo-count");
+
+var todos = [];
+
+// Displaying  time
+function displayTime() {
+  var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
+  timeDisplayEl.text(rightNow);
+}
+setInterval(displayTime, 1000);
+
+// The following function renders items in a todo list as <li> elements
+function renderTodos() {
+  // Clear todoList element and update todoCountSpan
+  todoList.innerHTML = "";
+  todoCountSpan.textContent = todos.length;
+}
+
+// This function is being called below and will run when the page loads.
+function init() {
+  // Get stored todos from localStorage
+  var storedTodos = JSON.parse(localStorage.getItem("todos"));
+
+  // If todos were retrieved from localStorage, update the todos array to it
+  if (storedTodos !== null) {
+    todos = storedTodos;
+  }
+
+  // This is a helper function that will render todos to the DOM
+  renderTodos();
+}
+
+function storeTodos() {
+  // Stringify and set key in localStorage to todos array
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// Add submit event to form
+todoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var todoText = todoInput.value.trim();
+
+  // Return from function early if submitted todoText is blank
+  if (todoText === "") {
+    return;
+  }
+
+  // Add new todoText to todos array, clear the input
+  todos.push(todoText);
+  todoInput.value = "";
+
+  // Store updated todos in localStorage, re-render the list
+  storeTodos();
+  //renderTodos();
+});
+
+// Add click event to todoList element
+saveButton.addEventListener("click", function (event) {
+  var element = event.target;
+
+  // Checks if element is a button
+  //if (element.matches("button") === true) {
+    // Get its data-index value and remove the todo element from the list
+   // var index = element.parentElement.getAttribute("data-index");
+   // todos.splice(index, 1);
+
+    // Store updated todos in localStorage, re-render the list
+    storeTodos();
+    renderTodos();
+  }
+});
+
+// Calls init to retrieve data and render it to the page on load
+init();
